df5344cff43c6b6bd7285f082969ed28910e9dfb
HuayiL <442488254@qq.com>
2025-03-03 10:02:51 +0800

    add double buffer prototype

--- gputiled.cu
+++ gputiled.cu
@@ -25,6 +25,9 @@ bool checkAccuracy(float * C, float * Ref, int size){
 	return true;
 }
 
+#define LOAD_FLOAT4(A,B) *reinterpret_cast<float4*>(&A) = *reinterpret_cast<const float4*>(&B)
+#define LOAD_FLOAT2(A,B) *reinterpret_cast<float2*>(&A) = *reinterpret_cast<const float2*>(&B)
+
 template <int TS> __global__ void gputiled0(r_Ptr<float> C, cr_Ptr<float> A, cr_Ptr<float> B,int Ay,int Ax,int Bx)
 {
 	__shared__ float Atile[TS][TS];  // tile in A eg [16][16]
@@ -236,12 +239,8 @@ template <int TS, int TM> __global__ void gputiled3(r_Ptr<float> C, cr_Ptr<float
 	}
 }
 
-
-
-
 template <int TS, int TM> __global__ void gputiled4(r_Ptr<float> C, cr_Ptr<float> A, cr_Ptr<float> B,int Ay,int Ax,int Bx)
 {
-	//further work including double buffering(by mem.async instr in ptx), removing register conflict in SASS, warptiling, manipulate the warp scheduling and others.
 	static_assert(TM%4==0&&TM>0,"A bad TM");
 	static_assert(TS%TM==0&&TS>TM,"A bad TS");
 	// A block still deals with TS*TS elements.
@@ -270,15 +269,17 @@ template <int TS, int TM> __global__ void gputiled4(r_Ptr<float> C, cr_Ptr<float
 	for(int t=0; t<gridDim.x; t++){
 		// load TS*TS matrix to smem.
 		for(int m=0;m<TM;m++){
-		    for(int n=0;n<TM;n++){
-		    	//There is back conflict here but it is worth for removing bank conflicts later.
-		    	Atile[tx*TM+n][ty*TM+m] = A[(ay+m)*Ax+ax+n];  // copy A tile to shared mem with transpose
-		    }
-		}
-		for(int m=0;m<TM;m++){
-		    for(int n=0;n<TM/4;n++){
-		    	reinterpret_cast<float4*>(Btile)[(ty*TM+m)*TS/4+tx*TM/4+n] = reinterpret_cast<const float4*>(B)[(by+m)*Bx/4+bx/4+n]; // copy B tile to shared mem
-		    }
+			for(int n=0;n<TM/4;n++){
+				//There is back conflict here but it is worth for removing bank conflicts later.
+				// copy A tile to shared mem with transpose
+				float4 tmp;
+				LOAD_FLOAT4(tmp,A[(ay+m)*Ax+ax+4*n]);
+				Atile[tx*TM+4*n  ][ty*TM+m] = tmp.x;  
+				Atile[tx*TM+4*n+1][ty*TM+m] = tmp.y; 
+				Atile[tx*TM+4*n+2][ty*TM+m] = tmp.z; 
+				Atile[tx*TM+4*n+3][ty*TM+m] = tmp.w; 
+				LOAD_FLOAT4(Btile[ty*TM+m][tx*TM+n*4],B[(by+m)*Bx+bx+n*4]);// copy B tile to shared mem
+			}
 		}
 		__syncthreads();
 		
@@ -289,8 +290,8 @@ template <int TS, int TM> __global__ void gputiled4(r_Ptr<float> C, cr_Ptr<float
 			// here we split the TM*TM^TM*TM matmul by spliting into vectors mult.
 			for(int m=0;m<TM;m++){
 				for(int n=0;n<TM/4;n++){
-					reinterpret_cast<float4*>(regA)[n] = reinterpret_cast<float4*>(Atile)[(k*TM+m)*TS/4+ty*TM/4+n];
-			    	reinterpret_cast<float4*>(regB)[n] = reinterpret_cast<float4*>(Btile)[(k*TM+m)*TS/4+tx*TM/4+n];
+					LOAD_FLOAT4(regA[4*n],Atile[k*TM+m][ty*TM+n*4]);
+					LOAD_FLOAT4(regB[4*n],Btile[k*TM+m][tx*TM+n*4]);
 				}
 				// deal with TM elements in one column.
 				for(int i=0;i<TM;i++){
@@ -307,8 +308,95 @@ template <int TS, int TM> __global__ void gputiled4(r_Ptr<float> C, cr_Ptr<float
 
 #pragma unroll 16
 	for(int m=0;m<TM;m++){
-		for(int n=0;n<TM;n++){
-			C[(ay+m)*Bx+bx+n] = threadResults[m][n]; // store complete result
+		for(int n=0;n<TM/4;n++){
+			LOAD_FLOAT4(C[(ay+m)*Bx+bx+4*n],threadResults[m][4*n]);// store complete result
+		}
+	}
+}
+
+template <int TS, int TM> __global__ void gputiled5(r_Ptr<float> C, cr_Ptr<float> A, cr_Ptr<float> B,int Ay,int Ax,int Bx)
+{
+	// Warning: this kind of double buffer here does not make perf improvement.
+	// further work including double buffering(by mem.async instr in ptx), removing register conflict in SASS, warptiling, manipulate the warp scheduling and others.
+	static_assert(TM%4==0&&TM>0,"A bad TM");
+	static_assert(TS%TM==0&&TS>TM,"A bad TS");
+	
+	int doubleBufferIdx = 0;
+
+	// A block still deals with TS*TS elements.
+	// using double buffer here.
+	// each turn deal with matirx A with shape[]
+	__shared__ float Atile[TS*2][TS];  // tile in A eg [32][32]
+	__shared__ float Btile[TS*2][TS];  // tile in B eg [32][32]
+
+	// A warp deals with 32*TM*TM elements.
+	// A thread deals with TM*TM elements.
+	float threadResults[TM][TM] = {0.0};
+	float regA[TM] = {0.0};
+	float regB[TM] = {0.0};
+
+	int tx  = threadIdx.x;            // tile col index j
+	int ty  = threadIdx.y;            // tile row index i
+	int ocx = blockDim.x * blockIdx.x * TM;  // tile x origin in C (all threads), tiling along row
+	int ocy = blockDim.y * blockIdx.y * TM;  // tile y origin in C (all threads), tiling along column
+	
+	// denote that this thread deal with a TM*TM matrix starts from [tx*TM, ty*TM]
+	// in an TS*TS (for C) matrix starts from [ocx,ocy].
+	int ax = tx*TM;      // j or x in first tile on A
+	int by = ty*TM;   // i or y in first tile on B
+	int ay = ocy+by;  // i or y in first tile on A and C
+	int bx = ocx+ax;  // j or x in first tile on B and C
+
+#pragma unroll 16
+	for(int t=0; t<=gridDim.x; t++){
+		// load TS*TS matrix to smem.
+		int loadOffset = doubleBufferIdx*TS;
+		int computeOffset = (1^doubleBufferIdx)*TS;
+		if(t<gridDim.x){
+			for(int m=0;m<TM;m++){
+				for(int n=0;n<TM/4;n++){
+					//There is back conflict here but it is worth for removing bank conflicts later.
+					// copy A tile to shared mem with transpose
+					float4 tmp;
+					LOAD_FLOAT4(tmp,A[(ay+m)*Ax+ax+4*n]);
+					Atile[tx*TM+4*n  +loadOffset][ty*TM+m] = tmp.x;  
+					Atile[tx*TM+4*n+1+loadOffset][ty*TM+m] = tmp.y; 
+					Atile[tx*TM+4*n+2+loadOffset][ty*TM+m] = tmp.z; 
+					Atile[tx*TM+4*n+3+loadOffset][ty*TM+m] = tmp.w; 
+					LOAD_FLOAT4(Btile[ty*TM+m+loadOffset][tx*TM+n*4],B[(by+m)*Bx+bx+n*4]);// copy B tile to shared mem
+				}
+			}
+		}
+
+		// the outer(k) loop is like the loop of t.
+		for(int k=0;t && k<TS/TM;k++) {
+			// here we deal with a sub-matrix for subA with index starts from [ty*TM,k*TM]
+			// and a sub-matrix for subB with index starts from [k*TM,tx*TM]
+			// here we split the TM*TM^TM*TM matmul by spliting into vectors mult.
+			for(int m=0;m<TM;m++){
+				for(int n=0;n<TM/4;n++){
+					LOAD_FLOAT4(regA[4*n],Atile[k*TM+m+computeOffset][ty*TM+n*4]);
+					LOAD_FLOAT4(regB[4*n],Btile[k*TM+m+computeOffset][tx*TM+n*4]);
+				}
+				// deal with TM elements in one column.
+				for(int i=0;i<TM;i++){
+					for(int j=0;j<TM;j++){
+						threadResults[i][j] += regA[i] * regB[j];
+					}
+				}
+			}
+		}
+		__syncthreads();
+		ax += TS;         // step A tiles along rows of A
+		by += TS;         // step B tiles down  cols of B
+		doubleBufferIdx ^= 1;
+		
+	}
+
+#pragma unroll 16
+	for(int m=0;m<TM;m++){
+		for(int n=0;n<TM/4;n++){
+			LOAD_FLOAT4(C[(ay+m)*Bx+bx+4*n],threadResults[m][4*n]);
 		}
 	}
 }
@@ -397,6 +485,21 @@ int main(int argc,char *argv[])
 		cx::timer tim;
 		for(int k=0;k<nacc;k++){
 			if(tilex == 32) gputiled4<32,TM><<<blocks,threads>>>(dev_C.data().get(),dev_A.data().get(),dev_B.data().get(),Arow,Acol,Bcol);
+			else if(tilex == 16) gputiled4<16, TM><<<blocks,threads>>>(dev_C.data().get(),dev_A.data().get(),dev_B.data().get(),Arow,Acol,Bcol);
+			else if(tilex == 8 ) gputiled4<8, TM><<<blocks,threads>>>(dev_C.data().get(),dev_A.data().get(),dev_B.data().get(),Arow,Acol,Bcol);
+		}
+		cudaDeviceSynchronize();
+		t3 = tim.lap_ms()/(double)(nacc);
+	} else if(kernel_index == 5){
+		constexpr int TM = 4;
+		dim3 threads = {tilex/TM, tilex/TM, 1}; //force square
+		dim3 blocks =  {(Bcol+threads.x-1)/tilex,(Arow+threads.x-1)/tilex,1};
+
+		cx::timer tim;
+		for(int k=0;k<nacc;k++){
+			if(tilex == 32) gputiled5<32,TM><<<blocks,threads>>>(dev_C.data().get(),dev_A.data().get(),dev_B.data().get(),Arow,Acol,Bcol);
+			else if(tilex == 16) gputiled5<16, TM><<<blocks,threads>>>(dev_C.data().get(),dev_A.data().get(),dev_B.data().get(),Arow,Acol,Bcol);
+			else if(tilex == 8 ) gputiled5<8, TM><<<blocks,threads>>>(dev_C.data().get(),dev_A.data().get(),dev_B.data().get(),Arow,Acol,Bcol);
 		}
 		cudaDeviceSynchronize();
 		t3 = tim.lap_ms()/(double)(nacc);
